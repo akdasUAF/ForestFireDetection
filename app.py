7b4940a250d90747df37749f4325806d63f88ed3
Luis Avalo <65528180+lavalo98@users.noreply.github.com>
2023-02-03 13:11:16 -0900

    Comments

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 
+# Import Python Imaging Library PIL(Pillow)
 from PIL import Image
 
 from keras.utils import load_img
@@ -20,11 +21,14 @@ def predict():
     imageFile = request.files['imageFile']
     image_path = "./images/" + imageFile.filename
     imageFile.save(image_path)
+    # Grab Image
     img = Image.open(image_path)
 
+    # Get and Print Width and Height
     width = img.width
     height = img.height
 
+    # Re-sizing the image to fit into the models input
     userImage = load_img(image_path, target_size=(224,224))
     userImage = img_to_array(userImage)
     userImage = userImage.reshape((1, userImage.shape[0], userImage.shape[1], userImage.shape[2]))
@@ -35,6 +39,7 @@ def predict():
 
     classification = '%s (%.2f%%)' % (label[1], label[2]*100)
 
+    # Returning the main page to the user with variables to use on the front end
     return render_template("index.html", prediction = classification, width = width, height = height)
 
 
