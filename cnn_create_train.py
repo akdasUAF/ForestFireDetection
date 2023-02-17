7a6a87201baad5d4b4c419bef2863cb8bd0446ec
lbunderwood <lbunderwood@alaska.edu>
2023-02-16 16:43:22 -0900

    added exporting/importing of cnn

--- cnn_create_train.py
+++ cnn_create_train.py
@@ -4,12 +4,9 @@
 # forest fires in images
 
 import tensorflow as tf
-import torch
-from matplotlib import pyplot as plt
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Conv2D, Dropout, Flatten, MaxPooling2D
 import sys
-import os
 
 
 def import_dataset(image_size):
@@ -31,38 +28,6 @@ def import_dataset(image_size):
     return train_ds, validation_ds, test_ds
 
 
-def reshape(data):
-    # Reshaping the array to 4-dims so that it can work with the Keras API
-    data = data.reshape(data.shape[0], 28, 28, 1)
-
-    # Making sure that the values are float so that we can get decimal points after division
-    data = data.astype('float32')
-
-    # Normalizing the RGB codes by dividing it to the max RGB value.
-    data /= 255
-
-    return data
-
-
-def visualize(data, label, title):
-    figure = plt.figure(figsize=(10, 8))
-    plt.title(title)
-    
-    cols, rows = 5, 5
-    num_samples = cols * rows
-    train_samples = data[:num_samples, :, :]
-
-    print(f'{label} shape:', data.shape)
-    print(f'Number of images in {label}', data.shape[0])
-
-    for i in range(0, cols * rows):
-        img = train_samples[i]
-        figure.add_subplot(rows, cols, i + 1)
-        plt.axis("off")
-        plt.imshow(tf.squeeze(img), cmap="gray")
-    plt.show()
-
-
 def create_model(input_shape):
     # Creating a Sequential Model and adding the layers
     model = Sequential()
@@ -72,38 +37,38 @@ def create_model(input_shape):
     model.add(Dense(128, activation=tf.nn.relu))
     model.add(Dropout(0.2))
     model.add(Dense(1,activation='sigmoid'))
-
-    model.build()
-    model.summary()
+    
+    model.compile(optimizer='adam', 
+                loss='binary_crossentropy', 
+                metrics=['accuracy'])
     return model
 
 
 def train(model, train_ds, validation_ds):
-    model.compile(optimizer='adam', 
-                loss='binary_crossentropy', 
-                metrics=['accuracy'])
     model.fit(x=train_ds, validation_data=validation_ds, epochs=2)
     return model
 
 
-def evaluate(model, dataset):
-    model.evaluate(x=dataset)
+def save_model(model, path):
+    model.save_weights(path)
 
 
 def main():
+
+    # setup
     image_size = (254, 254)
-
-    # get the data
     train_ds, validation_ds, test_ds = import_dataset(image_size)
-
-    # check GPU availability
     print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 
+    # create
     model = create_model(image_size + (3, ))
+    model.summary()
 
+    # train
     model = train(model, train_ds, validation_ds)
 
-    evaluate(model, test_ds)
+    # save
+    save_model(model, './forest_fire_cnn')
 
     return 0
 
