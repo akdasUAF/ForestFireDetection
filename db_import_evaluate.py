b30378a7c3898e6c3dedf2a81e972065c978ffa2
lbunderwood <lbunderwood@alaska.edu>
2023-04-07 19:43:04 +0000

    replaced DBN with version2 from Yali

--- db_import_evaluate.py
+++ db_import_evaluate.py
@@ -1,10 +1,33 @@
-import tensorflow as tf
-from tensorflow.keras.preprocessing.image import ImageDataGenerator, DirectoryIterator
-
-tf.disable_v2_behavior()
-
-import glob2 as gb
 import numpy as np
-from sklearn.model_selection import train_test_split
-import db_create_train as dbct
+import sys
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from sklearn.neural_network import BernoulliRBM
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from joblib import dump, load
+import db_create_train
 
+def DBN_import_model(model_filename):
+    loaded_pipeline = load(model_filename)
+    return loaded_pipeline
+    print(f'Model loaded from {model_filename}')
+
+def DBN_evaluate_model(pipeline, x_test, y_test):
+    predictions = pipeline.predict(x_test)
+    accuracy = accuracy_score(y_test, predictions)
+    print(f"Accuracy = {accuracy}")
+
+def main():
+    image_width_in_pixels = 254
+    image_height_in_pixels = 254
+    image_size = (image_width_in_pixels, image_height_in_pixels)
+
+    _, _, x_test, y_test = db_create_train.DBN_import_dataset(image_size)
+    pipeline = DBN_import_model('./Models/weights/dbn_pipeline_model.joblib')
+    DBN_evaluate_model(pipeline, x_test, y_test)
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
